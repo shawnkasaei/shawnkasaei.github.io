b7ed4639081afc49d98ca3c21313cc7499ec85a0
Shawn <shayankasaei@gmail.com>
2024-09-14 20:34:26 +0330

    v0.5.2.6

--- javascript/script.js
+++ javascript/script.js
@@ -6,7 +6,7 @@ var config = {
     streamingAssetsUrl: "StreamingAssets",
     companyName: "PAZH PLANET",
     productName: "PAZH COIN",
-    productVersion: "0.5.2.5",
+    productVersion: "0.5.2.6",
   };
 var container = document.querySelector("#unity-container");
 var canvas = document.querySelector("#unity-canvas");
