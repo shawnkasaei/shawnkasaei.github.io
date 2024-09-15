a18af64d0b916cbb033c0ad8a095540999d5c639
Shawn <shayankasaei@gmail.com>
2024-09-15 23:25:18 +0330

    v0.5.3.3

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,12 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCFTjmT2AKkcB7_IodwCDHye0OpJA1jN2I",
-  authDomain: "pazh-9da5e.firebaseapp.com",
-  projectId: "pazh-9da5e",
-  storageBucket: "pazh-9da5e.appspot.com",
-  messagingSenderId: "208774180749",
-  appId: "1:208774180749:web:9d21688ee09369c06c57c4",
-  measurementId: "G-MYS5FGLM9K"
-};
-
-const db = firebaseApp.firestore();
-const auth = firebaseApp.auth();
-
 var buildUrl = "Build";
 var config = {
-    dataUrl: buildUrl + "/fdcb3f3fa29447487cfc8931ec41a1fa.data",
+    dataUrl: buildUrl + "/238ec352a3595bc50c60a1958d940ec5.data",
     frameworkUrl: buildUrl + "/5a04318a1ad056fa003a20a480f2ed27.js",
     codeUrl: buildUrl + "/74f3356836ff29e972fdb7f0c4cbcdab.wasm",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "PAZH PLANET",
     productName: "PAZH COIN",
-    productVersion: "0.5.3.2",
+    productVersion: "0.5.3.3",
   };
 var container = document.querySelector("#unity-container");
 var canvas = document.querySelector("#unity-canvas");
