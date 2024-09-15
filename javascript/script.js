d075b8255ff281c75a9412a36074a52645c233cb
Shawn <shayankasaei@gmail.com>
2024-09-15 17:00:35 +0330

    v0.5.2.10

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,12 @@
 var buildUrl = "Build";
 var config = {
-    dataUrl: buildUrl + "/9ca88780304728dd46da64347d56f3a1.data",
-    frameworkUrl: buildUrl + "/fad1a8ac27451f942030423074d5558a.js",
-    codeUrl: buildUrl + "/9ff93fc5fdcab8c1a010fe8416de83e3.wasm",
+    dataUrl: buildUrl + "/b9f1f9cb26396871c7a8ed03af3ba15d.data.gz",
+    frameworkUrl: buildUrl + "/fad1a8ac27451f942030423074d5558a.js.gz",
+    codeUrl: buildUrl + "/9ff93fc5fdcab8c1a010fe8416de83e3.wasm.gz",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "PAZH PLANET",
     productName: "PAZH COIN",
-    productVersion: "0.5.2.9",
+    productVersion: "0.5.2.10",
   };
 var container = document.querySelector("#unity-container");
 var canvas = document.querySelector("#unity-canvas");
