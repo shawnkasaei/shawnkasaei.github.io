2be0ba40ab334390240ec011fa2778c9391182a8
Shawn <shayankasaei@gmail.com>
2024-09-16 21:31:37 +0330

    v0.5.3.6d

--- v0.5.3.5/javascript/script.js
+++ v0.5.3.5/javascript/script.js
@@ -1,18 +1,17 @@
 var buildUrl = "Build";
 var config = {
-    dataUrl: buildUrl + "/e38632e99a048a5c3cbab272cba75de5.data",
-    frameworkUrl: buildUrl + "/5a04318a1ad056fa003a20a480f2ed27.js",
-    codeUrl: buildUrl + "/74f3356836ff29e972fdb7f0c4cbcdab.wasm",
+    dataUrl: buildUrl + "/151cc8b204d9d169962890919282c246.data",
+    frameworkUrl: buildUrl + "/a4c134c9974c56d2aef8742a88751793.js",
+    codeUrl: buildUrl + "/d25bc3019efbd793e0cf42ff7a8c332a.wasm",
     streamingAssetsUrl: "StreamingAssets",
     companyName: "PAZH PLANET",
     productName: "PAZH COIN",
-    productVersion: "0.5.3.5",
+    productVersion: "0.5.3.6",
   };
 var container = document.querySelector("#unity-container");
 var canvas = document.querySelector("#unity-canvas");
 var loadingBar = document.querySelector("#unity-loading-bar");
 var progressBarFull = document.querySelector("#unity-progress-bar-full");
-var gameInstance = null;
 
 if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
     config.devicePixelRatio = 1;
@@ -20,14 +19,14 @@ if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
 
 loadingBar.style.display = "block";
 
-var loaderUrl = buildUrl + "/v0.5.3.4.loader.js";
+var loaderUrl = buildUrl + "/v0.5.3.5.loader.js";
 var script = document.createElement("script");
 script.src = loaderUrl;
 script.onload = () => {
     createUnityInstance(canvas, config, (progress) => {
         progressBarFull.style.width = 100 * progress + "%";
     }).then((unityInstance) => {
-        gameInstance = unityInstance;
+        window.unityInstance = unityInstance;
         loadingBar.style.display = "none";
         
         onGameLoaded();
@@ -39,5 +38,5 @@ script.onload = () => {
 document.body.appendChild(script);
 
 function onGameLoaded() {
-    gameInstance.SendMessage("TelegramIntegration", "SetUserInfo", window.Telegram.WebApp.initData);
+  window.unityInstance.SendMessage("TelegramIntegration", "SetUserInfo", window.Telegram.WebApp.initData);
 }
